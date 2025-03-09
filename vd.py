b3614a2dd05ac82269052fb3768f27c23d4cb8ae
HayimShaul <hayim.shaul@gmail.com>
2025-03-09 21:34:06 +0200

    nicer code

--- vd.py
+++ vd.py
@@ -7,23 +7,28 @@ import project
 import z_dist
 import primitives
 
+# find the center point in a 2d cell
+def find_center_point(c):
+    if c[0] == False:
+        x = c[1] - 1
+    elif c[1] == False:
+        x = c[1] + 1
+    else:
+        x = (c[0] + c[1]) / 2
+
+    if c[2] == False:
+        return project.project(Point3D(x, 0, 0), c[3], 'y') + Point3D(0, -1, 0)
+    elif c[3] == False:
+        return project.project(Point3D(x, 0, 0), c[2], 'y') + Point3D(0, 1, 0)
+    else:
+        y1 = project.project(Point3D(x, 0, 0), c[2], 'y')
+        y2 = project.project(Point3D(x, 0, 0), c[3], 'y')
+        return Point3D(x, (y1 + y2) / 2, 0)
+
+
 # compute the 2d vertical decomposition on a plane p.
 # intersections is the list of all intersection segments of all pairs of planes.
-def vd2d(p : Plane, intersections, planes):
-    # compute all cells that p is a floor to
-    incidents = []
-    above = []
-    p_segs = []
-    for i in intersections:
-        if z_dist.is_incident(i, p):
-            incidents.append(i)
-            p_segs.append(i)
-    
-        if z_dist.is_above(i, p, 'z'):
-            if z_dist.is_directly_above(i, p, planes, 'z'):
-                above.append(i)
-                p_segs.append(project.project(i, p, 'z'))
-
+def vd2d(p : Plane, p_segs):
     p_points = []
     for i in p_segs:
         p_points.extend(primitives.endpoints(i))
@@ -31,7 +36,6 @@ def vd2d(p : Plane, intersections, planes):
     for i in range(len(p_segs)):
         for j in range(i+1, len(p_segs)):
             p_points.extend(intersection.intersect(p_segs[i], p_segs[j]))
-
 
     for p in p_points:
         # find the segment that is y-above p
@@ -51,7 +55,7 @@ def vd2d(p : Plane, intersections, planes):
             p_segs.extend(new_segs)
 
 
-    cells = []
+    cells2d = []
     for s in p_segs:
         if isinstance(s, Segment3D):
             x_floor = min(s.p1.x, s.p2.x)
@@ -70,32 +74,16 @@ def vd2d(p : Plane, intersections, planes):
         y_ceil_seg = z_dist.find_directly_above(mid_point, p_segs, 'y')
         y_ceil = Line3D(Point3D(y_ceil_seg.p1.x, y_ceil_seg.p1.y, 0), Point3D(y_ceil_seg.p2.x, y_ceil_seg.p2.y, 0))
 
-        center_point = (s.p1 + s.p2 + y_ceil_seg.p1 + y_ceil_seg.p2) / 4
-        z_floor = p
-        z_ceil = z_dist.find_directly_above(center_point, planes, 'z')
-
-        cells.append( [x_floor, x_ceil, y_floor, y_ceil, z_floor, z_ceil])
+        cells2d.append( [x_floor, x_ceil, y_floor, y_ceil] )
 
         # check if s is on the lower envelope induced on p
         y_abyss = z_dist.find_directly_below(mid_point, p_segs, 'y')
-        # check if p is on the lower envelope
-        z_abyss = z_dist.find_directly_below(center_point, planes, 'z')
-
-        # if p is on the lower envelope
-        if z_abyss == None:
-            cells.append( [x_floor, x_ceil, y_floor, y_ceil, False, p])
 
         # if s is on the lower envelope induced on p, add another cell
         if y_abyss == None:
-            center_point = mid_point + (0, -1, 0)
-            center_point = project.project(center_point, p, 'z')
-            z_ceil = z_dist.find_directly_above(center_point, planes, 'z')
-            cells.append( [x_floor, x_ceil, False, y_floor, p, z_ceil])
+            cells2d.append( [x_floor, x_ceil, False, y_floor] )
 
-            if z_abyss == None:
-                cells.append( [x_floor, x_ceil, False, y_floor, False, p])
-
-    return cells
+    return cells2d
 
 def vd(planes):
     intersection_lines = []
@@ -140,10 +128,25 @@ def vd(planes):
     
     cells = []
     for p in planes:
-        cells2d = vd2d(...)
-        for each c in cells2d:
-            add a cell that is c is its z-floor - find its z-ceil
+        p_segs = []
+        for i in intersections:
+            if z_dist.is_incident(i, p):
+                p_segs.append(i)
+        
+            if z_dist.is_above(i, p, 'z'):
+                if z_dist.is_directly_above(i, p, planes, 'z'):
+                    p_segs.append(project.project(i, p, 'z'))
 
-            if c is on the lower envelope:
-                add a cell that is c is its z-ceil - with no z-floor
+        cells2d = vd2d(p, p_segs)
+
+        for c in cells2d:
+            center_point = find_center_point(c)
+            center_point = project.project(center_point, p, 'z')
+
+            plane_above = z_dist.find_directly_above(center_point, planes, 'z')
+            cells.append( [c[0], c[1], c[2], c[3], p, plane_above ] )
+            plane_below = z_dist.find_directly_below(center_point, planes, 'z')
+            if plane_below == False:
+                cells.append( [c[0], c[1], c[2], c[3], False, p ] )
+
 
